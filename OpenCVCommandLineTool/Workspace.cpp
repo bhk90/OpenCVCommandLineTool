341f8fdb70780a83f34fac7a4eff3b56918dca51
H <user@example.com>
2025-04-17 10:40:18 +0800

    docs(Workspace): change constructor example comments

--- OpenCVCommandLineTool/Workspace.cpp
+++ OpenCVCommandLineTool/Workspace.cpp
@@ -7,7 +7,7 @@
 /// 
 ///			// 通过图像路径创建一个 MyImage 的 unique_ptr 对象，放入 workspace 中
 ///			std::unique_ptr<Workspace> workspace; 
-///			workspace = std::make_unique<Workspace>(std::make_unique<MyImage>("image.jpg"));
+///			workspace = std::make_unique<Workspace>("image.jpg");
 /// 
 ///		
 ///		 二、Workspace的功能：
